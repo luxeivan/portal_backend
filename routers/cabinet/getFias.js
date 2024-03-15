ccdf7d5933d685f3aca0482c64af8daf290181c7
luxeivan <luxeivan@gmail.com>
2024-03-15 15:05:07 +0300

    body -> query in fiasget

--- routers/cabinet/getFias.js
+++ routers/cabinet/getFias.js
@@ -1,6 +1,6 @@
 const { default: axios } = require('axios');
 const express = require('express');
-const { check, validationResult, checkSchema,body } = require("express-validator");
+const { check, validationResult, checkSchema, body, param, query } = require("express-validator");
 const getFias = express.Router();
 const url = "http://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
 
@@ -9,7 +9,7 @@ const token = process.env.DADATA_TOKEN
 
 
 getFias.get('/',
-   body('searchString').notEmpty(),
+    query('searchString').notEmpty(),
 
     async (req, res) => {
         // Проверка-------------------------
@@ -18,7 +18,7 @@ getFias.get('/',
             return res.json({ status: "error", message: "Пустое поле поиска" })
         }
         // -------------------------
-        const { searchString } = req.body
+        const { searchString } = req.query
         const options = {
             mode: "cors",
             headers: {
@@ -29,7 +29,7 @@ getFias.get('/',
         }
         axios.post(url, { query: searchString }, options)
             .then(result => {
-                console.log(result.data)
+                //console.log(result.data)
                 res.json({ status: "ok", data: result.data.suggestions })
             })
             .catch(error => {
