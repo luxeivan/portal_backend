0e78108cb897c0b9323450808ebfd1056644d382
luxeivan <luxeivan@gmail.com>
2024-04-16 09:56:02 +0300

    add console zvonok

--- services/sendCodeToPhone.js
+++ services/sendCodeToPhone.js
@@ -4,6 +4,7 @@ const campaign_id = '989295860'
 
 const sendCodeToPhone = async (phone) => {
     const response = await axios.get(`https://zvonok.com/manager/cabapi_external/api/v1/phones/flashcall/?campaign_id=${campaign_id}&phone=${phone}&public_key=${public_key}`)
+    console.log(response.data.data)
     return response.data.data.pincode
 }
 
