b197fe7d55be5ff6dd98e086f0c85ac7e2cb8fce
luxeivan <luxeivan@gmail.com>
2024-04-17 16:13:30 +0300

    добавил запрос на наименование подразделения по коду

--- routers/cabinet.js
+++ routers/cabinet.js
@@ -8,6 +8,7 @@ const uploadFile = require('./cabinet/uploadFile');
 const getFile = require('./cabinet/getFile');
 const getFias = require('./cabinet/getFias');
 const getInn = require('./cabinet/getInn');
+const getFmsUnit = require('./cabinet/getFmsUnit');
 // ... и так далее для всех остальных файлов в папке cabinet ...
 
 router.use('/subjects', subjectsRouter);
@@ -17,6 +18,7 @@ router.use('/upload-file', uploadFile);
 router.use('/get-file', getFile);
 router.use('/get-fias', getFias);
 router.use('/get-inn', getInn);
+router.use('/get-fms', getFmsUnit);
 // ... и так далее для всех остальных импортированных роутеров ...
 
 module.exports = router;
---
+++ routers/cabinet/getFmsUnit.js
@@ -0,0 +1,38 @@
+const { default: axios } = require('axios');
+const express = require('express');
+const { check, validationResult, checkSchema, body, param, query } = require("express-validator");
+const getFmsUnit = express.Router();
+const url = "http://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fms_unit";
+
+const token = process.env.DADATA_TOKEN
+getFmsUnit.get('/',
+    query('searchString').notEmpty(),
+
+    async (req, res) => {
+        // Проверка-------------------------
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.json({ status: "error", message: "Пустое поле поиска" })
+        }
+        // -------------------------
+        const { searchString } = req.query
+        const options = {
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": "Token " + token
+            },
+        }
+        axios.post(url, { query: searchString }, options)
+            .then(result => {
+                //console.log(result.data)
+                res.json({ status: "ok", data: result.data.suggestions })
+            })
+            .catch(error => {
+                console.log("error", error)
+                res.json({ status: "error", message: "ошибка получения организации" })
+            });
+    })
+
+module.exports = getFmsUnit;
