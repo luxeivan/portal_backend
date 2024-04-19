649d7bd0c70bf6eb6187337277a002d72eec1459
luxeivan <luxeivan@gmail.com>
2024-04-19 15:08:09 +0300

    pincode

--- services/sendCodeToPhone.js
+++ services/sendCodeToPhone.js
@@ -4,11 +4,11 @@ const campaign_id = '989295860'
 
 
 // const sendCodeToPhone = async (phone) => {
-    //     const response = await axios.get(`https://zvonok.com/manager/cabapi_external/api/v1/phones/flashcall/?campaign_id=${campaign_id}&phone=${phone}&public_key=${public_key}`)
-    //     console.log(response.data.data)
-    //     return response.data.data.pincode
-    // }
-    const sendCodeToPhone = async (phone) => {
+//     const response = await axios.get(`https://zvonok.com/manager/cabapi_external/api/v1/phones/flashcall/?campaign_id=${campaign_id}&phone=${phone}&public_key=${public_key}`)
+//     console.log(response.data.data)
+//     return response.data.data.pincode
+// }
+const sendCodeToPhone = async (phone) => {
     const authMegafon = process.env.AUTH_MEGAFON_SMS
     var pincode = Math.floor(1000 + Math.random() * 9000);
     const response = await axios.post('https://a2p-api.megalabs.ru/sms/v1/sms', {
@@ -22,8 +22,8 @@ const campaign_id = '989295860'
         },
     })
     if (response.data.result.status.code === 0) {
-        return pincodepincode
-    }else{
+        return pincode
+    } else {
         throw new Error('Не удалось отправить код')
     }
     // console.log(response.data)
