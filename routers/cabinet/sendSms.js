44c2f1099b17afc8da2c78248aaf5e351696d33f
luxeivan <luxeivan@gmail.com>
2024-04-19 15:20:19 +0300

    выключил отправку смс

--- routers/cabinet/sendSms.js
+++ routers/cabinet/sendSms.js
@@ -9,31 +9,32 @@ sendSms.get('/',
     query('to').notEmpty(),
     query('message').notEmpty(),
     async function (req, res) {
-         // Проверка-------------------------
-         const result = validationResult(req);
-         if (!result.isEmpty()) {
-             return res.json({ status: "error", message: "Нехватает нужных полей" })
-         }
-         // -------------------------
-        const userId = req.userId
-        try {
-            console.log(req.query)
-            const response = await axios.post('https://a2p-api.megalabs.ru/sms/v1/sms', {
-                from: "M-OBLENERGO",
-                to: Number(req.query.to),
-                message: req.query.message,
-            }, {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": authMegafon
-                },
-            })
-            console.log(response.data)
-            res.json({ status: "ок", message: "СМС отправлена" }); // Set disposition and send it.
-        } catch (error) {
-            console.log(error)
-            res.json({ status: "error", message: "Ошибка отправки СМС" });
-        }
-    })
+        //  // Проверка-------------------------
+        //  const result = validationResult(req);
+        //  if (!result.isEmpty()) {
+        //      return res.json({ status: "error", message: "Нехватает нужных полей" })
+        //  }
+        //  // -------------------------
+        // const userId = req.userId
+        // try {
+        //     console.log(req.query)
+        //     const response = await axios.post('https://a2p-api.megalabs.ru/sms/v1/sms', {
+        //         from: "M-OBLENERGO",
+        //         to: Number(req.query.to),
+        //         message: req.query.message,
+        //     }, {
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //             "Authorization": authMegafon
+        //         },
+        //     })
+        //     console.log(response.data)
+        //     res.json({ status: "ок", message: "СМС отправлена" }); // Set disposition and send it.
+        // } catch (error) {
+        //     console.log(error)
+        //     res.json({ status: "error", message: "Ошибка отправки СМС" });
+        // }
+    }
+)
 
 module.exports = sendSms;
