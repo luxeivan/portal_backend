53750d51cbbcc52d2e5204881000fc6c8edc529b
Stanislav <yanut995@gmail.com>
2024-04-25 10:08:00 +0300

    Стас маленько чинил app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
@@ -21,32 +22,41 @@ const options = {
   cert: fs.readFileSync("./ssl/luxeivan.ru_cert.pem"),
   key: fs.readFileSync("./ssl/luxeivan.ru_private_key.pem"),
 };
-const secretSession = process.env.SECRET_SESSION
-const port = process.env.PORT
-const portSSL = process.env.PORT_SSL
+const secretSession = process.env.SECRET_SESSION;
+const port = process.env.PORT;
+const portSSL = process.env.PORT_SSL;
 
 const app = express();
 app.use(cors({ credentials: true, origin: true }));
 app.use(
-    session({
-        cookie: {
-            httpOnly: true,
-            maxAge: 100000,
-            sameSite: 'none',
-            domain: 'luxeivan.ru',
-            secure: true
-        },
-        store: new FileStore({ retries: 1 }),
-        secret: secretSession,
-        saveUninitialized: true,
-    })
-)
-app.use(bodyParser.json())
-app.use(fileUpload({
+  session({
+    cookie: {
+      httpOnly: true,
+      maxAge: 100000,
+      sameSite: "none",
+      domain: "luxeivan.ru",
+      secure: true,
+    },
+    store: new FileStore({ retries: 1 }),
+    secret: secretSession,
+    // Опция resave: false говорит, что сессия не должна быть сохранена заново,
+    // если она не изменялась в ходе запроса, что уменьшает количество операций записи.
+    resave: false,
+    // Опция saveUninitialized: false говорит, что сессия не будет сохраняться,
+    // если она не инициализирована (т.е., не была изменена), что снижает нагрузку
+    // на хранилище сессий и предотвращает ненужные операции записи.
+    saveUninitialized: false,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(
+  fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
     useTempFiles: true,
-    tempFileDir: '/tmp/'
-}))
+    tempFileDir: "/tmp/",
+  })
+);
 
 //app.use(express.urlencoded({ extended: true }));
 
@@ -58,6 +68,6 @@ app.use("/api/cabinet", checkAuth, cabinet);
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(options, app);
 httpServer.listen(port, () => {
-    console.log('Listen port 5000...')
+  console.log(`Зашли и вышли, приключения на ${port} порту`);
 });
 httpsServer.listen(portSSL);
