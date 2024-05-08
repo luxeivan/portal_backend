f389134c3309bad9760c44905187fe6efcf9edd2
Stanislav <yanut995@gmail.com>
2024-05-08 11:41:03 +0300

    Сделал getCadastral

--- routers/cabinet.js
+++ routers/cabinet.js
@@ -1,28 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const subjectsRouter = require('./cabinet/subjects');
-const relationsRouter = require('./cabinet/relation');
-const objectsRouter = require('./cabinet/objects');
-const profileRouter = require('./cabinet/profile');
-const uploadFile = require('./cabinet/uploadFile');
-const getFile = require('./cabinet/getFile');
-const getFias = require('./cabinet/getFias');
-const getInn = require('./cabinet/getInn');
-const getFmsUnit = require('./cabinet/getFmsUnit');
-const sendSms = require('./cabinet/sendSms');
+const subjectsRouter = require("./cabinet/subjects");
+const relationsRouter = require("./cabinet/relation");
+const objectsRouter = require("./cabinet/objects");
+const profileRouter = require("./cabinet/profile");
+const uploadFile = require("./cabinet/uploadFile");
+const getFile = require("./cabinet/getFile");
+const getFias = require("./cabinet/getFias");
+const getInn = require("./cabinet/getInn");
+const getCadastral = require("./cabinet/getCadastral");
+const getFmsUnit = require("./cabinet/getFmsUnit");
+const sendSms = require("./cabinet/sendSms");
 // ... и так далее для всех остальных файлов в папке cabinet ...
 
-router.use('/subjects', subjectsRouter);
-router.use('/relations', relationsRouter);
-router.use('/objects', objectsRouter);
-router.use('/profile', profileRouter);
-router.use('/upload-file', uploadFile);
-router.use('/get-file', getFile);
-router.use('/get-fias', getFias);
-router.use('/get-inn', getInn);
-router.use('/get-fms', getFmsUnit);
-router.use('/send-sms', sendSms);
+router.use("/subjects", subjectsRouter);
+router.use("/relations", relationsRouter);
+router.use("/objects", objectsRouter);
+router.use("/profile", profileRouter);
+router.use("/upload-file", uploadFile);
+router.use("/get-file", getFile);
+router.use("/get-fias", getFias);
+router.use("/get-inn", getInn);
+router.use("/get-cadastral", getCadastral);
+router.use("/get-fms", getFmsUnit);
+router.use("/send-sms", sendSms);
 // ... и так далее для всех остальных импортированных роутеров ...
 
 module.exports = router;
---
+++ routers/cabinet/getCadastral.js
@@ -0,0 +1,39 @@
+const { default: axios } = require("axios");
+const express = require("express");
+const {
+  validationResult,
+  query,
+} = require("express-validator");
+const getCadastral = express.Router();
+const url = "https://cleaner.dadata.ru/api/v1/clean/address";
+
+const token = process.env.DADATA_TOKEN;
+getCadastral.get("/", query("addressObject").notEmpty(), async (req, res) => {
+  // Проверка-------------------------
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    return res.json({ status: "error", message: "Пустое поле поиска" });
+  }
+  // -------------------------
+  const { addressObject } = req.query;
+  const options = {
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Token " + token,
+    },
+  };
+  axios
+    .post(url, { query: addressObject }, options)
+    .then((result) => {
+      //console.log(result.data)
+      res.json({ status: "ok", data: result.data });
+    })
+    .catch((error) => {
+      console.log("error", error);
+      res.json({ status: "error", message: "ошибка получения организации" });
+    });
+});
+
+module.exports = getCadastral;
