8a390269ab65f6980598a4ef26ef815a7e6b639f
Stanislav <yanut995@gmail.com>
2024-05-16 14:00:27 +0300

    Добавил put в objects.js

--- routers/cabinet/objects.js
+++ routers/cabinet/objects.js
@@ -77,25 +77,22 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-// router.delete("/:id", async (req, res) => {
-//   try {
-//     const userId = req.userId;
-//     const idObject = req.params.id;
-//     const object = await getObjectItem(idObject);
-//     console.log(objectbject.attributes.profil.data.id);
-//     if (object.attributes.profil.data.id === userId) {
-//       const statusDel = await deleteObjectItem(idObject);
-//       res.json({
-//         status: "ok",
-//         message: `Объект ${statusDel.attributes.name} удален`,
-//       });
-//     } else {
-//       res.status(400).json({ status: "error", message: "Неверный id объекта" });
-//     }
-//   } catch (error) {
-//     //console.log(error.message)
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// });
+router.put("/:id", async (req, res) => {
+  try {
+    const userId = req.userId;
+    const idObject = req.params.id;
+    const { body } = req;
+    const object = await getObjectItem(idObject);
+    if (object.attributes.profil.data.id === userId) {
+      const updatedObject = await updateObjectItem(idObject, body);
+      res.json(updatedObject);
+    } else {
+      res.status(400).json({ status: "error", message: "Неверный id объекта" });
+    }
+  } catch (error) {
+    console.error("Ошибка обновления объекта:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 module.exports = router;
