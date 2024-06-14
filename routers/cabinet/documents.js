64e4ba0c29cbbd3fb75f15cce72248eb45d9035f
Stanislav <yanut995@gmail.com>
2024-06-14 08:28:01 +0300

    Сомнительно, но ок

--- routers/cabinet/documents.js
+++ routers/cabinet/documents.js
@@ -9,89 +9,7 @@ const { v4: uuidv4 } = require("uuid");
 const pathFileStorage = process.env.PATH_FILESTORAGE;
 const maxSizeFile = 10;
 
-const server1C = 'http://45.89.189.5/InfoBase/odata/standard.odata'
-
-// router.post("/upload", async function (req, res) {
-//   const uuid = uuidv4();
-//   const token = req.headers["authorization"].split(" ")[1];
-//   let userId;
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     userId = decoded.id; // Извлекаем ID пользователя из токена
-//   } catch (error) {
-//     return res
-//       .status(401)
-//       .json({ status: "error", message: "Невалидный токен" });
-//   }
-
-//   const dirName = `${pathFileStorage}/${userId}`;
-
-//   if (!req.files || Object.keys(req.files).length === 0) {
-//     return res.status(400).json({
-//       status: "error",
-//       message: "Нет файлов для загрузки",
-//       files: req.files,
-//     });
-//   }
-
-//   let bigFile = false;
-//   Object.keys(req.files).map((item) => {
-//     if (req.files[item].size > maxSizeFile * 1024 * 1024) {
-//       bigFile = true;
-//     }
-//   });
-
-//   if (bigFile) {
-//     return res
-//       .status(400)
-//       .json({ status: "error", message: "Файлы больше 10МБ не принимаются" });
-//   }
-
-//   try {
-//     await fs.promises.access(dirName);
-//   } catch (err) {
-//     if (err && err.code === "ENOENT") {
-//       try {
-//         await fs.promises.mkdir(dirName);
-//       } catch (error) {
-//         console.log(error);
-//         return res.status(500).json({
-//           status: "error",
-//           message: "Ошибка при создании директории для файлов",
-//         });
-//       }
-//     }
-//   }
-
-//   const arrayWriteFile = Object.keys(req.files).map((item) => {
-//     return new Promise(function (resolve, reject) {
-//       const filename = `${item}_${uuid}.${req.files[item].name.slice(
-//         req.files[item].name.lastIndexOf(".") + 1
-//       )}`;
-//       req.files[item].mv(`${dirName}/${filename}`, function (err) {
-//         if (err)
-//           reject({
-//             status: "error",
-//             message: "Ошибка при записи файлов",
-//             error,
-//           });
-//         resolve({ filename, path: `${dirName}/${filename}` });
-//       });
-//     });
-//   });
-
-//   Promise.all(arrayWriteFile)
-//     .then(async (responses) => {
-//       res.json({ status: "ok", files: responses });
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//       return res
-//         .status(500)
-//         .json({ status: "error", message: "Ошибка при записи файлов" });
-//     });
-//  });
+const server1C = "http://45.89.189.5/InfoBase/odata/standard.odata";
 
 router.post("/", async function (req, res) {
   const userId = req.userId;
@@ -114,7 +32,7 @@ router.post("/", async function (req, res) {
     Description: documentName,
     profile: userId,
     files: filesData,
-    nameDoc_Key
+    nameDoc_Key,
   };
 
   try {
@@ -149,11 +67,6 @@ router.get("/", async function (req, res) {
   try {
     const response = await axios.get(
       `${server1C}/Catalog_DocumentsOfProfiles?$format=json&$filter=(profile eq '${userId}') and (DeletionMark eq false)`
-      // {
-      //   headers: {
-      //     Authorization: `Bearer ${token}`,
-      //   },
-      // }
     );
 
     if (response.data) {
@@ -176,7 +89,7 @@ router.get("/getNameDocs", async function (req, res) {
     const response = await axios.get(
       `${server1C}/ChartOfCharacteristicTypes_НаименованиеДокументовПрофиля?$format=json`
     );
-    console.log(response.data)
+    console.log(response.data);
     if (response.data) {
       res.json({
         status: "ok",
@@ -190,7 +103,8 @@ router.get("/getNameDocs", async function (req, res) {
       message: "Ошибка при получении данных из 1С",
     });
   }
-})
+});
+
 router.get("/:id", async function (req, res) {
   const userId = req.userId;
   const idDocument = req.params.id;
@@ -198,7 +112,7 @@ router.get("/:id", async function (req, res) {
     const response = await axios.get(
       `${server1C}/Catalog_DocumentsOfProfiles(guid'${idDocument}')?$format=json`
     );
-    console.log(response.data)
+    console.log(response.data);
     if (response.data) {
       if (response.data.profile === userId) {
         res.json({
@@ -208,7 +122,7 @@ router.get("/:id", async function (req, res) {
       } else {
         res.status(400).json({
           status: "error",
-          message: "Не верный id документа"
+          message: "Не верный id документа",
         });
       }
     }
@@ -217,6 +131,58 @@ router.get("/:id", async function (req, res) {
     res.status(500).json({
       status: "error",
       message: "Ошибка при получении данных из 1С",
+    });
+  }
+});
+
+router.put("/:id", async function (req, res) {
+  const userId = req.userId;
+  const idDocument = req.params.id;
+  const { documentName, files, nameDoc_Key } = req.body;
+
+  if (!documentName && !files) {
+    return res.status(400).json({
+      status: "error",
+      message: "Нет нужных полей",
+    });
+  }
+
+  const filesData = files.map((file, index) => ({
+    LineNumber: index + 1,
+    fileName: file.name,
+    fileType: file.name.split(".").pop(),
+    fileSize: "1000",
+  }));
+
+  const payload = {
+    Description: documentName,
+    profile: userId,
+    files: filesData,
+    nameDoc_Key
+  };
+
+  try {
+    const response = await axios.patch(
+      `${server1C}/Catalog_DocumentsOfProfiles(guid'${idDocument}')?$format=json`,
+      payload,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response.data) {
+      res.json({
+        status: "ok",
+        message: "Документ успешно обновлен",
+        data: response.data,
+      });
+    }
+  } catch (error) {
+    console.error("Ошибка при обновлении данных в 1С", error);
+    res.status(500).json({
+      status: "error",
+      message: "Ошибка при обновлении данных в 1С",
     });
   }
 });
