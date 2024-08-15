83484d891615fc0e035bdeb48c454762b03c65ce
Stanislav <yanut995@gmail.com>
2024-08-15 10:12:34 +0300

    Пробуем поменять часовой пояс в логере

--- logger.js
+++ logger.js
@@ -1,9 +1,11 @@
 const { createLogger, format, transports } = require('winston');
 require('winston-daily-rotate-file');
-const moment = require('moment');
+const moment = require('moment-timezone');
+
+const timezone = 'Europe/Moscow';
 
 const logFormat = format.printf(({ level, message, timestamp, stack }) => {
-  const formattedTimestamp = moment(timestamp).format('HH:mm DD.MM.YYYY');
+  const formattedTimestamp = moment(timestamp).tz(timezone).format('HH:mm DD.MM.YYYY');
   return `${formattedTimestamp} ${level}: ${stack || message}`;
 });
 
@@ -38,3 +40,46 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 module.exports = logger;
+
+
+
+// const { createLogger, format, transports } = require('winston');
+// require('winston-daily-rotate-file');
+// const moment = require('moment');
+
+// const logFormat = format.printf(({ level, message, timestamp, stack }) => {
+//   const formattedTimestamp = moment(timestamp).format('HH:mm DD.MM.YYYY');
+//   return `${formattedTimestamp} ${level}: ${stack || message}`;
+// });
+
+// const dailyRotateFileTransport = new transports.DailyRotateFile({
+//   filename: 'logs/error-%DATE%.log',
+//   datePattern: 'YYYY-MM-DD',
+//   zippedArchive: true,
+//   maxSize: '20m',
+//   maxFiles: '30d',
+//   level: 'error',
+// });
+
+// const logger = createLogger({
+//   level: 'info',
+//   format: format.combine(
+//     format.timestamp(),
+//     format.errors({ stack: true }),
+//     format.splat(),
+//     logFormat
+//   ),
+//   defaultMeta: { service: 'user-service' },
+//   transports: [
+//     new transports.File({ filename: 'logs/combined.log' }),
+//     dailyRotateFileTransport
+//   ],
+// });
+
+// if (process.env.NODE_ENV !== 'production') {
+//   logger.add(new transports.Console({
+//     format: format.simple(),
+//   }));
+// }
+
+// module.exports = logger;
