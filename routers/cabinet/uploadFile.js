24ba9c632af3be3b0294ba5d77b4c7ffbaeb1e8e
Stanislav <yanut995@gmail.com>
2024-10-17 09:31:02 +0300

    вернул

--- routers/cabinet/uploadFile.js
+++ routers/cabinet/uploadFile.js
@@ -23,10 +23,6 @@ router.post("/", async function (req, res) {
   const userId = req.userId;
   const dirName = `${pathFileStorage}/${userId}`;
 
-  console.log(
-    `\n=== Upload File Route Hit ===\nUUID: ${uuid}\nUser ID: ${userId}\n`
-  );
-
   // Получение настроек обмена из 1С
   let user_Key, mainVolume_Key;
   try {
@@ -60,11 +56,6 @@ router.post("/", async function (req, res) {
     ? req.files.files
     : [req.files.files];
 
-  console.log(
-    "Полученные файлы:",
-    files.map((f) => f.name)
-  );
-
   // Получение допустимых расширений и максимального размера из 1С
   let allowedExtensions = [];
   let maxSizeFile = 10 * 1024 * 1024; // По умолчанию 10 МБ
@@ -72,14 +63,13 @@ router.post("/", async function (req, res) {
   console.log("Полученный categoryKey:", categoryKey); // Логируем полученный categoryKey
 
   try {
-    const requestUrl = `${SERVER_1C}/Catalog_services_categoriesFiles?$format=json&$filter=Ref_Key eq guid'${categoryKey}'`;
-    console.log("Запрос к 1С для получения данных категории:", requestUrl); // Логируем запрос
+    const requestUrl = `${SERVER_1C}/Catalog_services_categoriesFiles?$format=json&$filter=category_Key eq guid'${categoryKey}'&$expand=category`;
+    console.log("Запрос к 1С:", requestUrl); // Логируем запрос
     const response = await axios.get(requestUrl, { headers });
-    console.log("Ответ от 1С по категории:", response.data); // Логируем ответ от 1С
+    console.log("Ответ от 1С:", response.data); // Логируем ответ от 1С
 
     if (response.data.value && response.data.value.length > 0) {
-      const categoryData = response.data.value[0];
-      console.log("Полученные данные категории:", categoryData);
+      const categoryData = response.data.value[0].category;
       allowedExtensions = JSON.parse(categoryData.availableExtensionsJSON);
       maxSizeFile = parseInt(categoryData.maximumSize) * 1024 * 1024;
       console.log("Допустимые расширения из 1С:", allowedExtensions);
@@ -103,14 +93,11 @@ router.post("/", async function (req, res) {
   let invalidFile = false;
   for (const file of files) {
     const fileExtension = file.name.split(".").pop().toUpperCase();
-    console.log(`Проверка файла ${file.name}, расширение: ${fileExtension}`);
     if (!allowedExtensions.includes(fileExtension)) {
-      console.warn(`Недопустимое расширение файла: ${file.name}`);
       invalidFile = true;
       break;
     }
     if (file.size > maxSizeFile) {
-      console.warn(`Файл превышает максимальный размер: ${file.name}`);
       invalidFile = true;
       break;
     }
@@ -129,26 +116,23 @@ router.post("/", async function (req, res) {
   try {
     // Создаём директорию, если она не существует
     await fs.promises.mkdir(dirName, { recursive: true });
-    console.log(`Директория для сохранения файлов: ${dirName}`);
 
     // Сохраняем файлы во временную директорию
     for (const file of files) {
       const filePath = `${dirName}/${file.name}`;
       await file.mv(filePath);
-      console.log(`Файл сохранён: ${filePath}`);
     }
 
     // Создаем новый PDF-документ
     const pdfDoc = await PDFDocument.create();
-    console.log("Создан новый PDF-документ для объединения файлов");
 
     for (const file of files) {
       const filePath = `${dirName}/${file.name}`;
       const fileBuffer = await fs.promises.readFile(filePath);
-      console.log(`Обрабатываем файл: ${file.name}`);
 
+      let pdfImage;
       if (file.mimetype === "image/jpeg" || file.mimetype === "image/jpg") {
-        const pdfImage = await pdfDoc.embedJpg(fileBuffer);
+        pdfImage = await pdfDoc.embedJpg(fileBuffer);
         const page = pdfDoc.addPage([pdfImage.width, pdfImage.height]);
         page.drawImage(pdfImage, {
           x: 0,
@@ -156,9 +140,8 @@ router.post("/", async function (req, res) {
           width: pdfImage.width,
           height: pdfImage.height,
         });
-        console.log(`Добавлено изображение JPG: ${file.name}`);
       } else if (file.mimetype === "image/png") {
-        const pdfImage = await pdfDoc.embedPng(fileBuffer);
+        pdfImage = await pdfDoc.embedPng(fileBuffer);
         const page = pdfDoc.addPage([pdfImage.width, pdfImage.height]);
         page.drawImage(pdfImage, {
           x: 0,
@@ -166,7 +149,6 @@ router.post("/", async function (req, res) {
           width: pdfImage.width,
           height: pdfImage.height,
         });
-        console.log(`Добавлено изображение PNG: ${file.name}`);
       } else if (file.mimetype === "application/pdf") {
         const donorPdfDoc = await PDFDocument.load(fileBuffer);
         const donorPages = await pdfDoc.copyPages(
@@ -174,9 +156,7 @@ router.post("/", async function (req, res) {
           donorPdfDoc.getPageIndices()
         );
         donorPages.forEach((page) => pdfDoc.addPage(page));
-        console.log(`Добавлены страницы из PDF: ${file.name}`);
       } else {
-        console.warn(`Неизвестный тип файла, пропускаем: ${file.name}`);
         continue;
       }
     }
@@ -184,19 +164,11 @@ router.post("/", async function (req, res) {
     const pdfBytes = await pdfDoc.save();
     const pdfFilename = `combined_document_${uuid}.pdf`;
     const pdfPath = `${dirName}/${pdfFilename}`;
-    await fs.promises.writeFile(pdfPath, pdfBytes);
-    console.log(`Объединённый PDF сохранён: ${pdfPath}`);
 
-    // Удаление временных файлов
-    for (const file of files) {
-      const filePath = `${dirName}/${file.name}`;
-      try {
-        await fs.promises.unlink(filePath);
-        console.log(`Временный файл удалён: ${filePath}`);
-      } catch (err) {
-        console.error(`Не удалось удалить временный файл: ${filePath}`, err);
-      }
-    }
+    await fs.promises.writeFile(pdfPath, pdfBytes);
+    logger.info(
+      `Объединенный PDF успешно создан: ${pdfFilename}. UUID: ${uuid}`
+    );
 
     // Отправка файла в 1С
     try {
@@ -208,8 +180,7 @@ router.post("/", async function (req, res) {
         .toISOString()
         .slice(0, 10)
         .replace(/-/g, "");
-      const filePathIn1C = `claimsProject\\${formattedDate}\\${pdfFilename}`;
-      console.log("Путь к файлу в 1С:", filePathIn1C);
+      const filePathIn1C = `claimsProject/${formattedDate}/${pdfFilename}`; // Изменение: замена '\' на '/' для корректного формирования пути
 
       const payload = {
         Description: req.body.documentName,
@@ -217,7 +188,7 @@ router.post("/", async function (req, res) {
         Автор_Key: user_Key,
         ДатаМодификацииУниверсальная: currentDate.toISOString(),
         ДатаСоздания: currentDate.toISOString(),
-        ПутьКФайлу: filePathIn1C,
+        ПутьКФайлу: filePathIn1C, // Убедитесь, что путь корректен для чтения в 1С
         Размер: fileData.length.toString(),
         Расширение: "pdf",
         ТипХраненияФайла: "ВТомахНаДиске",
@@ -227,14 +198,12 @@ router.post("/", async function (req, res) {
         ФайлХранилище_Base64Data: base64File,
       };
 
-      console.log("Payload для загрузки в 1С:", payload);
-
       const uploadResponse = await axios.post(
         `${SERVER_1C}/Catalog_profileПрисоединенныеФайлы?$format=json`,
         payload,
         { headers }
       );
-      console.log("Файл успешно загружен в 1С:", uploadResponse.data);
+      console.log("Файл успешно загружен в 1С:", uploadResponse.data); // Логируем успешную загрузку
     } catch (error) {
       console.error(
         "Ошибка при отправке файла в 1С:",
@@ -249,14 +218,15 @@ router.post("/", async function (req, res) {
     // Удаляем объединенный PDF после загрузки
     try {
       await fs.promises.unlink(pdfPath);
-      console.log(`Объединённый PDF удалён: ${pdfPath}`);
     } catch (err) {
-      console.error(`Не удалось удалить объединённый PDF: ${pdfPath}`, err);
+      logger.error(
+        `Не удалось удалить объединенный PDF: ${pdfPath}. Ошибка: ${err.message}`
+      );
     }
 
     return res.json({ status: "ok", message: "Файл успешно загружен" });
   } catch (error) {
-    console.error(
+    logger.error(
       `Ошибка при обработке файлов. UUID: ${uuid}. Ошибка: ${error.message}`
     );
     return res.status(500).json({
