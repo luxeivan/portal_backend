6096833a43c0267f1f2420af13c07fd838d33d64
Stanislav <yanut995@gmail.com>
2024-10-22 10:22:56 +0300

    Эксперимент с загрузкой

--- routers/cabinet/documents.js
+++ routers/cabinet/documents.js
@@ -11,7 +11,7 @@ const logger = require("../../logger");
 const pathFileStorage = process.env.PATH_FILESTORAGE;
 const maxSizeFile = 10;
 
-const documentsStore = {}; // Ключ — userId, значение — массив документов пользователя
+const documentsStore = {}; 
 
 const SERVER_1C = process.env.SERVER_1C;
 const server1c_auth = process.env.SERVER_1C_AUTHORIZATION;
@@ -95,68 +95,6 @@ router.get("/categories", async function (req, res) {
  *         description: Ошибка при сохранении документа
  */
 
-// router.post("/", async function (req, res) {
-//   try {
-//     const userId = req.userId;
-//     const { documentName, files, nameDoc_Key } = req.body;
-
-//     if (!documentName && !files) {
-//       logger.error(
-//         `Отсутствуют необходимые поля в запросе от пользователя с id: ${userId}`
-//       );
-//       return res.status(400).json({
-//         status: "error",
-//         message: "Нет нужных полей",
-//       });
-//     }
-
-//     const filesData = files.map((file, index) => ({
-//       LineNumber: index + 1,
-//       fileName: file.name,
-//       fileType: file.name.split(".").pop(),
-//       fileSize: "1000",
-//     }));
-
-//     const payload = {
-//       Description: documentName,
-//       profile: userId,
-//       files: filesData,
-//       nameDoc_Key,
-//     };
-
-//     // logger.info(
-//     //   `Получен запрос на сохранение документа от пользователя с id: ${userId}`
-//     // );
-
-//     const response = await axios.post(
-//       `${server1C}/Catalog_DocumentsOfProfiles?$format=json`,
-//       payload,
-//       {
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       }
-//     );
-
-//     if (response.data) {
-//       logger.info(`Документ успешно сохранен для пользователя с id: ${userId}`);
-//       res.json({
-//         status: "ok",
-//         message: "Документ успешно сохранен",
-//         data: response.data,
-//       });
-//     }
-//   } catch (error) {
-//     // logger.error(
-//     //   `Ошибка при отправке данных в 1С для пользователя с id: ${userId}. Ошибка: ${error.message}`
-//     // );
-//     res.status(500).json({
-//       status: "error",
-//       message: "Ошибка при отправке данных в 1С",
-//     });
-//   }
-// });
-
 router.post("/", async function (req, res) {
   try {
     const userId = req.userId;
@@ -217,44 +155,6 @@ router.post("/", async function (req, res) {
  *       500:
  *         description: Ошибка при получении данных
  */
-
-// router.get("/", async function (req, res) {
-//   const userId = req.userId;
-
-//   // logger.info(
-//   //   `Получен запрос на получение документов пользователя с id: ${userId}`
-//   // );
-
-//   try {
-//     const response = await axios.get(
-//       `${server1C}/Catalog_DocumentsOfProfiles?$format=json&$filter=(profile eq '${userId}') and (DeletionMark eq false)`
-//     );
-
-//     if (response.data) {
-//       logger.info(
-//         `Документы успешно получены для пользователя с id: ${userId}`
-//       );
-//       return res.json({
-//         status: "ok",
-//         documents: response.data.value,
-//       });
-//     } else {
-//       logger.warn(`Нет данных для пользователя с id: ${userId}`);
-//       return res.status(404).json({
-//         status: "error",
-//         message: "Нет данных",
-//       });
-//     }
-//   } catch (error) {
-//     logger.error(
-//       `Ошибка при получении данных из 1С для пользователя с id: ${userId}. Ошибка: ${error.message}`
-//     );
-//     return res.status(500).json({
-//       status: "error",
-//       message: "Ошибка при получении данных из 1С",
-//     });
-//   }
-// });
 
 router.get("/", async function (req, res) {
   const userId = req.userId;
