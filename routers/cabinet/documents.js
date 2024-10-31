0cf74718e0061ef729056b272a54544fe27958d0
Stanislav <yanut995@gmail.com>
2024-10-31 12:15:14 +0300

    4321§

--- routers/cabinet/documents.js
+++ routers/cabinet/documents.js
@@ -146,6 +146,7 @@ router.get("/", async function (req, res) {
   const userId = req.userId;
 
   try {
+    //добавить + параметр в фильтр(Категории)
     const requestUrl = `${SERVER_1C}/InformationRegister_connectionsOfElements/SliceLast(,Condition='element2 eq cast(guid'${userId}', 'Catalog_profile')')?$format=json&$expand=element1&$filter=usage eq true`;
     // const requestUrl = `${SERVER_1C}/InformationRegister_connectionsOfElements?$format=json&$filter=element2 eq cast(guid'${userId}', 'Catalog_profile') and usage eq true and element1_Type eq 'StandardODATA.Catalog_profileПрисоединенныеФайлы'&$expand=element1`;
 
