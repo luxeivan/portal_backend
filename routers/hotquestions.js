6100d2bd576cb36507f08bed3fa64acff0a49891
Stanislav <yanut995@gmail.com>
2024-11-25 09:26:55 +0300

    Сделал 1C роутер на ГорячиеОтветы 18+

---
+++ routers/hotquestions.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+
+const router = express.Router();
+
+const SERVER_1C = process.env.SERVER_1C;
+const server1c_auth = process.env.SERVER_1C_AUTHORIZATION;
+
+const headers = {
+  Authorization: server1c_auth,
+};
+
+//Роутер на получение вопросов/ответов
+router.get("/", async (req, res) => {
+  try {
+    //Запрашиваем инфу
+    const hotQuestions = await axios.get(
+      `${SERVER_1C}/Catalog_quickAnswers/?$format=json`,
+      { headers }
+    );
+
+    // console.log("Проверяем что тут приходит", hotQuestions);
+
+    const questionsArray = hotQuestions.data.value;
+
+    // console.log("Проверяем что тут приходит", questionsArray);
+
+    const formattedQuestions = questionsArray.map((item) => ({
+      question: item.question,
+      answer: item.answer,
+    }));
+
+    // console.log("Проверяем что тут приходит", formattedQuestions);
+
+    res.status(200).json(formattedQuestions);
+  } catch (error) {
+    console.log(
+      "Ошибка при получении данных о 'Горячих ответах' из 1С:",
+      error
+    );
+    res.status(500).json({
+      message: "Ошибка при получении данных о 'Горячих ответах' из 1С",
+    });
+  }
+});
+
+module.exports = router;
