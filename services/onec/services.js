dd6533fbc6b4afa7c11c7295788f416eded74d06
Иван Шишкин <luxeivan@gmail.com>
2024-12-17 17:01:10 +0300

    Изменения по dadata

--- services/onec/services.js
+++ services/onec/services.js
@@ -242,6 +242,7 @@ const servicesOneC = {
         serviceItem.fields = resp[1].data.fields
         serviceItem.styles = resp[1].data.styles
         serviceItem.links = resp[1].data.links
+        serviceItem.externalService = resp[1].data.externalService
         serviceItem.versionId = resp[1].data.versionId
         // await Promise.all(resp[1].data.value.map(item => {
 
