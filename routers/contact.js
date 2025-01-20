675caf0e39ca0aa8917a0252bfb9c1ab6bdca397
Stanislav <yanut995@gmail.com>
2025-01-20 08:06:47 +0300

    123

--- routers/contact.js
+++ routers/contact.js
@@ -1,5 +1,3 @@
-// Файл: /Users/yanutstas/Desktop/Project/portal_backend/routers/contact.js
-
 const express = require("express");
 const axios = require("axios");
 require("dotenv").config();
@@ -76,6 +74,83 @@ router.get("/", async (req, res) => {
 });
 
 module.exports = router;
+
+// const express = require("express");
+// const axios = require("axios");
+// require("dotenv").config();
+
+// const router = express.Router();
+
+// // Данные для доступа к 1С
+// const SERVER_1C = process.env.SERVER_1C;
+// const server1c_auth = process.env.SERVER_1C_AUTHORIZATION;
+
+// // Адрес, по которому будем оповещать бота
+// // (если бот и бэкенд на одном сервере, указывайте http://127.0.0.1:3001/notifyError)
+// const botNotifyUrl =
+//   process.env.BOT_NOTIFY_URL || "http://127.0.0.1:3001/notifyError";
+
+// // Заголовки для авторизации на 1С
+// const headers = {
+//   Authorization: server1c_auth,
+// };
+
+// // Основной маршрут GET /api/contacts
+// router.get("/", async (req, res) => {
+//   try {
+//     // 1) Запрашиваем контактную инфу
+//     const contactInfoResponse = await axios.get(
+//       `${SERVER_1C}/InformationRegister_portalContactInformation?$format=json&$orderby=lineNum`,
+//       { headers }
+//     );
+
+//     // 2) Тянем фотки
+//     const photosResponse = await axios.get(
+//       `${SERVER_1C}/Catalog_РайоныЭлектрическихСетейПрисоединенныеФайлы?$format=json&$expand=Том&$select=Ref_Key,Description,ВладелецФайла_Key,ПутьКФайлу,РеквизитДопУпорядочивания,Том,ТипХраненияФайла,Том/ПолныйПутьWindows,ФайлХранилище&$filter=DeletionMark%20eq%20false`,
+//       { headers }
+//     );
+
+//     const contactInfo = contactInfoResponse.data.value;
+//     const photos = photosResponse.data.value;
+
+//     // 3) Склеиваем данные
+//     const combinedData = contactInfo.map((contact) => {
+//       // Находим все фотки для текущего контакта
+//       const matchedPhotos = photos.filter(
+//         (photo) => photo.ВладелецФайла_Key === contact.object
+//       );
+//       return {
+//         ...contact,
+//         photos: matchedPhotos.map((photo) => ({
+//           ПутьКФайлу: photo.ПутьКФайлу,
+//           ПолныйПутьWindows: photo.Том.ПолныйПутьWindows,
+//         })),
+//       };
+//     });
+
+//     // 4) Отдаём данные на фронт
+//     res.status(200).json(combinedData);
+//   } catch (error) {
+//     // 5) Пишем лог об ошибке
+//     console.error("Ошибка при получении данных из 1C:", error);
+
+//     // 6) Сообщаем фронту, что случилась ошибка
+//     res.status(500).json({ message: "Ошибка при получении данных из 1C" });
+
+//     // 7) Если настроен адрес бота, пытаемся его оповестить
+//     if (botNotifyUrl) {
+//       try {
+//         await axios.post(botNotifyUrl, {
+//           message: `Ошибка при получении данных из 1C: ${error.message}`,
+//         });
+//       } catch (notifyErr) {
+//         console.error("Не смогли оповестить бота:", notifyErr);
+//       }
+//     }
+//   }
+// });
+
+// module.exports = router;
 
 // const express = require("express");
 // const axios = require("axios");
