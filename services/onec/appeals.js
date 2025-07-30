535fa87400b6cbdc6480e55b6de1c73d3e8a52e6
luxeivan <luxeivan@gmail.com>
2025-07-30 21:06:54 +0300

    начал делать обращения

---
+++ services/onec/appeals.js
@@ -0,0 +1,79 @@
+const axios = require("axios");
+// const moment = require("moment");
+require("dotenv").config();
+
+// const server1c = process.env.SERVER_1C;
+const server1cHttpService = process.env.SERVER_1C_HTTP_SERVICE;
+const server1c_auth = process.env.SERVER_1C_AUTHORIZATION;
+
+
+const headers = {
+    Authorization: server1c_auth,
+};
+
+const appealsOneC = {
+    getAppealsList: async (id) => {
+        try {
+            const response = await axios.get(
+                `${server1cHttpService}/portalFields/tasks/${id}`,
+                {
+                    headers,
+                }
+            );
+            // console.log(response.data.data);
+
+            if (!response.data) {
+                return false;
+            }
+            return response.data.data;
+        } catch (error) {
+            return false;
+
+        }
+    },
+    getAppealsByClaims: async (userId, id) => {
+        try {
+            const response = await axios.get(
+                `${server1cHttpService}/profile/${userId}/tasks/${id}`,
+                {
+                    headers,
+                }
+            );
+            // console.log(response.data.data);
+
+            if (!response.data) {
+                return false;
+            }
+            return response.data.data;
+        } catch (error) {
+            return false;
+
+        }
+    },
+    createNewAppeals: async (userId, data) => {
+        try {
+            const response = await axios.post(
+                `${server1cHttpService}/profile/${userId}/claims/${data.claimId}/tasks`,
+                {
+                    ...data
+                },
+                {
+                    headers,
+                }
+            );
+            console.log(response.data);
+
+            if (!response.data) {
+                return false;
+            }
+            return response.data.data;
+        } catch (error) {
+            console.log(error);
+            return false;
+
+        }
+
+    }
+}
+
+module.exports = appealsOneC;
